c42466fb18b4d9e8d0b772419f1b27deea35008f
yontey <muasajohn01@gmail.com>
2019-02-15 21:51:44 +0300

    added the innerHTML properties

--- Js/script.js
+++ Js/script.js
@@ -2,19 +2,20 @@ function track() {
   var value1 =parseInt(document.getElementById('a').value);
   var value2 =parseInt(document.getElementById('b').value);
   var value3 =parseInt(document.getElementById('c').value);
+  var form = document.getElementById('form1')
 
   if (value1+value2 > value3 && value2+value3>value1 && value1+value3>value2){
     if (value1==value2 && value2==value3 && value1==value3) {
-      alert("THIS IS AN EQUILATERALL TRIANGLE")
+      document.getElementById('demo').innerHTML = 'This is an <strong>equillateral</strong> triangle <br> <img src="images/equillateral.png">'
     }
     else if (value1!==value2 && value2!=value3 && value1!==value3){
-    alert("THIS IS A SCALENE TRIANGLE")
+    document.getElementById('demo').innerHTML= 'This is a <strong>Scalene</strong> triangle <br> <img src="images/scalene.png">'
     }
     else{
-      alert("THE TRIANGLE IS ISOSCELESS")
+      document.getElementById('demo').innerHTML='This is a <strong>isoscel</strong> triangle <br> <img src="images/isosceless.png">'
     }
   }
   else{
-    alert("THIS IS NOT A TRIANGLE")
+    document.getElementById('demo').innerHTML ='This is <strong>NOT A TRIANGLE</strong> <br>  <img src="images/warning.png">'
   }
 }
