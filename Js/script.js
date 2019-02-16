2f9aa5462991028ff87daaaf045825c8cdb58cc3
yontey <muasajohn01@gmail.com>
2019-02-16 23:44:40 +0300

    enforcing integrity of entered values

--- Js/script.js
+++ Js/script.js
@@ -3,19 +3,24 @@ function track() {
   var value2 =parseInt(document.getElementById('b').value);
   var value3 =parseInt(document.getElementById('c').value);
 
-
-  if (value1+value2 > value3 && value2+value3>value1 && value1+value3>value2){
-    if (value1==value2 && value2==value3 && value1==value3) {
-      document.getElementById('demo').innerHTML = 'This is an <strong>equillateral</strong> triangle <br> <img src="images/equillateral.png">'
-    }
-    else if (value1!==value2 && value2!=value3 && value1!==value3){
-    document.getElementById('demo').innerHTML= 'This is a <strong>Scalene</strong> triangle <br> <img src="images/scalene.png">'
-    }
-    else{
-      document.getElementById('demo').innerHTML='This is a <strong>isoscel</strong> triangle <br> <img src="images/isosceless.png">'
-    }
+  if (value1 || value2 || value3 isNaN()) {
+    alert("PLEASE ENTER NUMERIC VALUES")
   }
   else{
-    document.getElementById('demo').innerHTML ='This is <strong>NOT A TRIANGLE</strong> <br>  <img src="images/warning.png">'
+    if (value1+value2 > value3 && value2+value3>value1 && value1+value3>value2){
+      if (value1==value2 && value2==value3 && value1==value3) {
+        document.getElementById('demo').innerHTML = 'This is an <strong>equillateral</strong> triangle <br> <img src="images/equillateral.png">'
+      }
+      else if (value1!==value2 && value2!=value3 && value1!==value3){
+      document.getElementById('demo').innerHTML= 'This is a <strong>Scalene</strong> triangle <br> <img src="images/scalene.png">'
+      }
+      else{
+        document.getElementById('demo').innerHTML='This is a <strong>isoscel</strong> triangle <br> <img src="images/isosceless.png">'
+      }
+    }
+    else{
+      document.getElementById('demo').innerHTML ='This is <strong>NOT A TRIANGLE</strong> <br>  <img src="images/warning.png">'
+    }
   }
+
 }
