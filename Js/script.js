21fd5813dd73381a2afdfde785a89a2ecbf92db6
yontey <muasajohn01@gmail.com>
2019-02-18 11:06:06 +0300

    changing image attribute

--- Js/script.js
+++ Js/script.js
@@ -2,7 +2,7 @@ function track() {
   var value1 =parseInt(document.getElementById('a').value);
   var value2 =parseInt(document.getElementById('b').value);
   var value3 =parseInt(document.getElementById('c').value);
-  
+
     if (value1+value2 > value3 && value2+value3>value1 && value1+value3>value2){
       if (value1==value2 && value2==value3 && value1==value3) {
         document.getElementById('demo').innerHTML = 'This is an <strong>equillateral</strong> triangle <br> <img src="images/equillateral.png">'
@@ -11,7 +11,7 @@ function track() {
       document.getElementById('demo').innerHTML= 'This is a <strong>Scalene</strong> triangle <br> <img src="images/scalene.png">'
       }
       else{
-        document.getElementById('demo').innerHTML='This is a <strong>isosceless</strong> triangle <br> <img src="images/isosceless.png">'
+        document.getElementById('demo').innerHTML='This is a <strong>isosceless</strong> triangle <br> <img src="images/isosceless.jpeg">'
       }
     }
     else{
